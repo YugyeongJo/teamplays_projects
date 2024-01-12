8ea273e65efcc6852bb6926f374e5e387e0e22c3
entangelk <kdtyohan@gmail.com>
2024-01-12 15:05:50 +0900

    update

--- models/member.py
+++ models/member.py
@@ -6,12 +6,11 @@ from pydantic import BaseModel, EmailStr
 class members(Document):
     user_ID: Optional[str] = None
     user_pswd: Optional[str] = None
-    user_email: Optional[EmailStr] = None    
+    user_email: Optional[str] = None    
     user_name: Optional[str] = None
     user_info : Optional[str] = None
-    user_birth : Optional[int] = None
+    user_birth : Optional[str] = None
     user_address : Optional[str] = None
-    user_phone : Optional[int] = None
     
   
     class Settings:
