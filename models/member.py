3ed17ff0dfa81b9b9f1e0ada7db5ec5c6284ae2e
rugkfl <kimgyoungha61@gmail.com>
2024-01-12 18:18:34 +0900

    update

--- models/member.py
+++ models/member.py
@@ -8,6 +8,7 @@ class members(Document):
     user_pswd: Optional[str] = None
     user_email: Optional[str] = None    
     user_name: Optional[str] = None
+    user_phone : Optional[str] = None
     user_info : Optional[str] = None
     user_birth : Optional[str] = None
     user_postcode : Optional[str] = None
