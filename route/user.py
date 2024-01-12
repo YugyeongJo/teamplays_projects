e7dc2b49ec62294dd39e353894f2db622326b2e9
entangelk <kdtyohan@gmail.com>
2024-01-12 10:21:57 +0900

    update

--- route/user.py
+++ route/user.py
@@ -14,42 +14,43 @@ collection_member = Database(members)
 
 templates = Jinja2Templates(directory="templates/")
 
-@router.get("user/user_join", response_class=HTMLResponse)
+@router.get("/user_join", response_class=HTMLResponse)
 async def user_join(request:Request):
     return templates.TemplateResponse(name="user/user_join.html", context={'request':request})
 
-@router.post("user/user_join", response_class=HTMLResponse)
+@router.post("/user_join", response_class=HTMLResponse)
 async def user_join(request:Request):
     return templates.TemplateResponse(name="user/user_join.html", context={'request':request})
 
-@router.get("user/user_login", response_class=HTMLResponse) 
+@router.get("/user_login", response_class=HTMLResponse) 
+async def user_login(request:Request):
+    request._query_params
+    return templates.TemplateResponse(name="user/user_login.html", context={'request':request})
+
+@router.post("/user_login", response_class=HTMLResponse) 
 async def user_login(request:Request):
     return templates.TemplateResponse(name="user/user_login.html", context={'request':request})
 
-@router.post("user/user_login", response_class=HTMLResponse) 
-async def user_login(request:Request):
-    return templates.TemplateResponse(name="user/user_login.html", context={'request':request})
-
-@router.get("user_mypage", response_class=HTMLResponse) 
+@router.get("/user_mypage", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_mypage.html", context={'request':request})
 
-@router.post("user_mypage", response_class=HTMLResponse) 
+@router.post("/user_mypage", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_mypage.html", context={'request':request})
 
-@router.get("user_infosearch", response_class=HTMLResponse) 
+@router.get("/user_infosearch", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_infosearch.html", context={'request':request})
 
-@router.post("user_infosearch", response_class=HTMLResponse) 
+@router.post("/user_infosearch", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_infosearch.html", context={'request':request})
 
-@router.get("user_privacypolicy", response_class=HTMLResponse) 
+@router.get("/user_privacypolicy", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_privacypolicy.html", context={'request':request})
 
-@router.post("user_privacypolicy", response_class=HTMLResponse) 
+@router.post("/user_privacypolicy", response_class=HTMLResponse) 
 async def mypage(request:Request):
     return templates.TemplateResponse(name="user_privacypolicy.html", context={'request':request})
