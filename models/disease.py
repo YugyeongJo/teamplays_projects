81cb1bd27929af119fcafce95d7ea7f8e92bb96f
entagelk <kdtyohan@gmail.com>
2024-01-13 21:57:03 +0900

    update

--- models/disease.py
+++ models/disease.py
@@ -4,16 +4,12 @@ from beanie import Document, Link
 from pydantic import BaseModel, EmailStr
 
 class diseases(Document):
-    # dise_KCD_code : Optional[str] = None
-    # dise_spc_code : Optional[int] = None
-    # dise_group : Optional[str] = None
-    # dise_name_kr: Optional[str] = None
-    # dise_name_en: Optional[str] = None
-    # dise_symp: Optional[str] = None
-    # dise_affected: Optional[str] = None
-    # dise_causes: Optional[str] = None
-    # dise_diagn : Optional[str] = None
-    # dise_cure : Optional[str] = None
+    dise_KCD_code : Optional[str] = None
+    dise_spc_code : Optional[int] = None
+    dise_group : Optional[str] = None
+    dise_name_kr: Optional[str] = None
+    dise_name_en: Optional[str] = None
+    dise_support: Optional[str] = None
     dise_url : Optional[str] = None
   
     class Settings:
