446841e8cf54ba10fe6814c0c222279bcf9200cf
rugkfl <kimgyoungha61@gmail.com>
2024-04-15 20:15:05 +0900

    update

--- route/info.py
+++ route/info.py
@@ -3,6 +3,7 @@ from starlette.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 
+
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates/")
